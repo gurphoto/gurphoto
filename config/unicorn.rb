08f2d7e1fa992f19977798627b70737b26917a35
gurphoto <bigguro@yandex.ru>
2013-05-15 23:34:24 +0400

    Исправил unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,12 +5,12 @@ preload_app true
 
 timeout 30
 
-listen "/home/user/projects/emerald_city/tmp/sockets/unicorn.sock", :backlog => 64
+listen "/home/user/projects/gurphoto/tmp/sockets/unicorn.sock", :backlog => 64
 
-pid "/home/user/projects/emerald_city/tmp/pids/unicorn.pid"
+pid "/home/user/projects/gurphoto/tmp/pids/unicorn.pid"
 
-stderr_path "/home/user/projects/emerald_city/log/unicorn.stderr.log"
-stdout_path "/home/user/projects/emerald_city/log/unicorn.stdout.log"
+stderr_path "/home/user/projects/gurphoto/log/unicorn.stderr.log"
+stdout_path "/home/user/projects/gurphoto/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
     defined?(ActiveRecord::Base) and
